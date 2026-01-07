4d9cb878518de44f471aef09900bfd06b7c6db35
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-07 23:38:46 +0200

    Fully remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import viteLogo from "/vite.svg";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Section from "./components/Section";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Section></Section>
-      <Footer></Footer>
     </>
   );
 }

922ef7f557c96b8f20f89976bb4efae33c0cf332
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-09 14:29:01 +0200

    Add state to tabs, onClick func into buttons + className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [activeTab, setActiveTab] = useState("personal");
+
   return (
     <header>
       <nav className="header-tab-navigation">
-        <button>Personal</button>
-        <button>Experience</button>
-        <button>Education</button>
-        <button>Skills</button>
+        <button
+          className={activeTab === "personal" ? "active" : ""}
+          onClick={() => setActiveTab("personal")}
+        >
+          Personal
+        </button>
+        <button
+          className={activeTab === "experience" ? "active" : ""}
+          onClick={() => setActiveTab("experience")}
+        >
+          Experience
+        </button>
+        <button
+          className={activeTab === "education" ? "active" : ""}
+          onClick={() => setActiveTab("education")}
+        >
+          Education
+        </button>
+        <button
+          className={activeTab === "skills" ? "active" : ""}
+          onClick={() => setActiveTab("skills")}
+        >
+          Skills
+        </button>
       </nav>
       <div className="header-download-btn">
         <button>Download</button>

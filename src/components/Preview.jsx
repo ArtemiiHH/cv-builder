9ecf8f56c913b0e117134f3407eea3814793f04d
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-11 14:08:28 +0200

    Split Preview into rows, columns

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,10 +2,41 @@ export default function Preview({ cvData }) {
   return (
     <div className="preview-window">
       <div className="cv-preview">
-        <h1 className="cv-name">{cvData.personal.fullName.toUpperCase()}</h1>
-        <h3 className="cv-profession">
-          {cvData.personal.profession.toUpperCase()}
-        </h3>
+        {/* Left Row */}
+        <div className="row-one">
+          {/* Name and Profession */}
+          <div className="name-profession">
+            {/* Full name */}
+            <h1 className="cv-name">
+              {cvData.personal.fullName.toUpperCase()}
+            </h1>
+            {/* Profession */}
+            <h3 className="cv-profession">
+              {cvData.personal.profession.toUpperCase()}
+            </h3>
+          </div>
+
+          {/* Profile */}
+          <div className="profile"></div>
+
+          {/* Work Experience */}
+          <div className="work-experience"></div>
+        </div>
+
+        {/* Right Row */}
+        <div className="row-two">
+          {/* Contact Details */}
+          <div className="contact-details"></div>
+
+          {/* Education Details */}
+          <div className="education-details"></div>
+
+          {/* Soft Skills */}
+          <div className="soft-skills"></div>
+
+          {/* Hard Skills */}
+          <div className="hard-skills"></div>
+        </div>
       </div>
     </div>
   );

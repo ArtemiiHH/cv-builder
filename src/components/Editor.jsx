79a22dfa0389e7216bab3064885fbc14db10557f
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-11 18:54:48 +0200

    Create state to check if form is open or not

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,8 +2,15 @@ import PersonalTab from "./PersonalTab";
 import ExperienceTab from "./ExperienceTab";
 import EducationTab from "./EducationTab";
 import SkillsTab from "./SkillsTab";
+import { useState } from "react";
 
 export default function Editor({ activeTab, cvData, updatePersonalInput }) {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  function displayForm() {
+    setIsFormOpen(true);
+  }
+
   if (activeTab === "personal") {
     return (
       <div className="editor-window">
@@ -16,7 +23,9 @@ export default function Editor({ activeTab, cvData, updatePersonalInput }) {
   } else if (activeTab === "experience") {
     return (
       <div className="editor-window">
-        <ExperienceTab></ExperienceTab>
+        <ExperienceTab
+          displayForm={displayForm}
+        ></ExperienceTab>
       </div>
     );
   } else if (activeTab === "education") {
